3c32697157ab35f420726caecb0ebab5d1ef522c
Lalita Kumari <barnwal6201@gmail.com>
2026-07-21 16:48:25 +0530

    Add login, signup, movie pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Movie from './pages/Movie'
 
 const App = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/movies' element={<Movie />} />
     </Routes>
     </>
   )
---
+++ frontend/src/pages/Movie.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MoviesPage from '../components/MoviesPage'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Movie = () => {
+  return (
+    <div>
+        <Navbar />
+      <MoviesPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default Movie
